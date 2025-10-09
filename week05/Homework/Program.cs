8e27099519a2d863cf2520822f817ec1cbd26ec2
Rodolfo Recordon <rodolforecordon@gmail.com>
2025-10-09 01:57:01 +0100

    week05 inheritance homework activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -2,8 +2,19 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Homework Project.");
-    }
+  static void Main(string[] args)
+  {
+    Assignment homework1 = new Assignment("Samuel Bennett", "Multiplication");
+    Console.WriteLine(homework1.GetSummary());
+    Console.WriteLine();
+
+    MathAssignment homework2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+    Console.WriteLine(homework2.GetSummary());
+    Console.WriteLine(homework2.GetHomeworkList());
+    Console.WriteLine();
+
+    WritingAssignment homework3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+    Console.WriteLine(homework3.GetSummary());
+    Console.WriteLine(homework3.GetWritingInformation());
+  }
 }
