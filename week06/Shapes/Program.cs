952196a8764b61d0b269ed5d97049dc489068bab
Rodolfo Recordon <rodolforecordon@gmail.com>
2025-10-14 00:18:54 +0100

    shapes activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    List<Shape> shapes = new List<Shape>();
+
+    shapes.Add(new Square("red", 3.5));
+    shapes.Add(new Rectangle("blue", 4.5, 10));
+    shapes.Add(new Circle("yellow", 5));
+
+    foreach (Shape shape in shapes)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+      Console.WriteLine($"Color: {shape.GetColor()} --- Area: {shape.GetArea()}");
     }
+  }
 }
